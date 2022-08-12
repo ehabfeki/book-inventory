498e9c34ad0704b46311df8701312dc89a401811
ehab.shaaban@contentsquare.com <ehab.shaaban@contentsquare.com>
2022-08-12 11:26:12 +0200

    chore: fmt

--- src/Book.ts
+++ src/Book.ts
@@ -1,12 +1,12 @@
- export  class  Book {
-    public isbn : string ;
-    public title : string ;
-    public subtitle: string;
-    public author: string;
-    public published : string;
-    public publisher: string;
-    public  pages: number;
-    public  description:string;
-    public  website: string;
-    public  copies: number;
+export class Book {
+  public isbn: string;
+  public title: string;
+  public subtitle: string;
+  public author: string;
+  public published: string;
+  public publisher: string;
+  public pages: number;
+  public description: string;
+  public website: string;
+  public copies: number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,121 +1,126 @@
-import express from 'express'
-import {Book} from './Book';
-import bodyParser from 'body-parser'
+import express from 'express';
+import { Book } from './Book';
+import bodyParser from 'body-parser';
 let books: Book[] = require('../data/books.json').books;
-const bookSchema = require('../src/schemas/book.json')
-import ajv from "ajv";
+const bookSchema = require('../src/schemas/book.json');
+import ajv from 'ajv';
 import { error } from 'console';
 
 const app = express();
 const port = 8080; // default port to listen
 const avjValidator = new ajv();
-const  jsonBodyParser = bodyParser.json({ type: 'application/*+json' });
+const jsonBodyParser = bodyParser.json({ type: 'application/*+json' });
 // define a route handler for the default home page
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    res.send("welcome to our book inventory");
+app.get('/', (req, res) => {
+  res.send('welcome to our book inventory');
 });
-app.get("/books", (req, res) => {
-    res.send(JSON.stringify(books, null, 4));
-})
+app.get('/books', (req, res) => {
+  res.send(JSON.stringify(books, null, 4));
+});
 
-
-app.put("/books",(req,res) =>{
-
-    if(req.body instanceof  Array) {
-        const givenBooks : Book[] = req.body;
-        const  invalidValues = givenBooks.some((book) => {
-            return !avjValidator.validate(bookSchema, book)
-        });
-        if(invalidValues) {
-            res.status(500);
-            res.send("At least one member is not a valid book");
-        }
-        else {
-            books = books.concat(givenBooks);
-            res.send(givenBooks.map((book) =>{return book.title}).join(',') + ' were added to the book inventory');
-        }
+app.put('/books', (req, res) => {
+  if (req.body instanceof Array) {
+    const givenBooks: Book[] = req.body;
+    const invalidValues = givenBooks.some((book) => {
+      return !avjValidator.validate(bookSchema, book);
+    });
+    if (invalidValues) {
+      res.status(500);
+      res.send('At least one member is not a valid book');
     } else {
-        const  book : Book = req.body;
-        const isValid: boolean = avjValidator.validate(bookSchema,book) ;
-        if(isValid) {
-            res.status(200);
-            res.send(`${book.title} was added to the inventory `)
-        }else  {
-            res.status(500);
-            console.log(avjValidator.errors[0].message)
-            res.send( (book.title ?`${book.title} is not valid book ` : ' book object is not well formated => ') + avjValidator.errors[0].message) ;
-        }
+      books = books.concat(givenBooks);
+      res.send(
+        givenBooks
+          .map((book) => {
+            return book.title;
+          })
+          .join(',') + ' were added to the book inventory'
+      );
     }
-
-})
+  } else {
+    const book: Book = req.body;
+    const isValid: boolean = avjValidator.validate(bookSchema, book);
+    if (isValid) {
+      res.status(200);
+      res.send(`${book.title} was added to the inventory `);
+    } else {
+      res.status(500);
+      console.log(avjValidator.errors[0].message);
+      res.send(
+        (book.title
+          ? `${book.title} is not valid book `
+          : ' book object is not well formated => ') +
+          avjValidator.errors[0].message
+      );
+    }
+  }
+});
 
 app.put('/books/:isbn', (req, res) => {
-    // fetch book match by `isbn`
-    const found = books.find( (book) => {
-        return book.isbn === req.params.isbn;
-    });
+  // fetch book match by `isbn`
+  const found = books.find((book) => {
+    return book.isbn === req.params.isbn;
+  });
 
-    // check if book found
-    if (found) {
-        const updated = {
-            isbn: found.isbn,
-            title: req.body.title,
-            copies: req.body.copies,
-            subtitle: req.body.subtitle,
-            author: req.body.author,
-            published: req.body.published,
-            publisher: req.body.publisher,
-            pages: req.body.pages,
-            description: req.body.description,
-            website: req.body.website,
-        };
+  // check if book found
+  if (found) {
+    const updated = {
+      isbn: found.isbn,
+      title: req.body.title,
+      copies: req.body.copies,
+      subtitle: req.body.subtitle,
+      author: req.body.author,
+      published: req.body.published,
+      publisher: req.body.publisher,
+      pages: req.body.pages,
+      description: req.body.description,
+      website: req.body.website
+    };
 
-        // find index of found object from array of data
-        const targetIndex = books.indexOf(found);
+    // find index of found object from array of data
+    const targetIndex = books.indexOf(found);
 
-        // replace object from data list with `updated` object
-        books.splice(targetIndex, 1, updated);
+    // replace object from data list with `updated` object
+    books.splice(targetIndex, 1, updated);
 
-        // return with status 204
-        res.status(204);
-        res.send(found.isbn + " is updated!")
-    }
-    else {
-        res.status(404);
-        res.send("owpzy daisy! something bad happend!")
-    }
+    // return with status 204
+    res.status(204);
+    res.send(found.isbn + ' is updated!');
+  } else {
+    res.status(404);
+    res.send('owpzy daisy! something bad happend!');
+  }
 });
 
 app.delete('/books/:isbn', (req, res) => {
-    const isbn = req.params.isbn;
-    books = books.filter(book => book.isbn !== isbn)
-    res.send("successfully deleted book with isbn code " + isbn);
-})
+  const isbn = req.params.isbn;
+  books = books.filter((book) => book.isbn !== isbn);
+  res.send('successfully deleted book with isbn code ' + isbn);
+});
 
 app.get('/search', (req, res) => {
-    const filters = req.query;
+  const filters = req.query;
 
-    const filteredBooks = books.filter(book => {
-
-      let isValid = true;
-      for (const key in filters) {
-          if(filters.hasOwnProperty(key)){
-              isValid = isValid && book[key as keyof typeof book] === filters[key];
-          }
+  const filteredBooks = books.filter((book) => {
+    let isValid = true;
+    for (const key in filters) {
+      if (filters.hasOwnProperty(key)) {
+        isValid = isValid && book[key as keyof typeof book] === filters[key];
       }
-      
-      return isValid;
-    });
+    }
 
-    res.send(filteredBooks);
-})
+    return isValid;
+  });
+
+  res.send(filteredBooks);
+});
 
 app.use(jsonBodyParser);
 
 // start the Express server
 app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+  console.log(`server started at http://localhost:${port}`);
 });
